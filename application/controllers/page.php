02e4bc41b4f878ed00c42f26c1734f5a9263e30e
xiaobin80 <tdtc_hrb@163.com>
2016-03-08 23:48:42 +0800

    Add a private function _loadSubView(). It extracts the function in the public section of the class.

--- application/controllers/page.php
+++ application/controllers/page.php
@@ -33,7 +33,11 @@ class Page extends Frontend_Controller {
 	public function search($searchVal) {
 		$this->data['isSearch'] = TRUE;
 		$this->data['searchVal'] = $searchVal;
-		$this->get_article_view('articles', $searchVal, 'article_list', FALSE);
+		$this->get_article_view(
+				'articles', 
+				$searchVal, 
+				'article_list',
+				FALSE);
 	}
 	
 	
@@ -46,7 +50,11 @@ class Page extends Frontend_Controller {
 		
 		if ($iCount) $iLevel = 3;
 		
-		$this->data['breadData'] = $this->get_navInstr($this->get_space($param), $this->data['lang_id'], $iLevel);
+		$this->data['breadData'] = $this->get_navInstr(
+				$this->get_space($param), 
+				$this->data['lang_id'], $iLevel);
+		
+		$templateName = 'templates/';
 		
 		if ($iCount) {
 			$strPrefix = substr($param, 0, $iCount);
@@ -55,62 +63,78 @@ class Page extends Frontend_Controller {
 			switch ($strPrefix) {
 				case 'Product': 
 					$this->_product_view($strName);
+
+					switch ($this->data['flag_iLang']) {
+						case 0:
+							$templateName .= 'production_cn';
+							break;
+						case 1:
+							$templateName .= 'production';
+							break;
+						case 2:
+							$templateName .= 'production_ja';
+							break;
+					}
+					
+					$this->_loadSubView($templateName);
+					
 					break;
 				case 'Company': 
 					$this->_company_view($strName);
 					break;
 				case 'Contact':
 					$this->_contact_view($strName);
+					$templateName .= 'contact';
+					$this->_loadSubView($templateName);
+					
 					break;
 				case 'About':
 					if ($strName === 'profile')
 						$this->get_article_view($strName, $strName, $strName, TRUE);
-					else
+					else {
 						$this->_about_view($strName);
+						$templateName .= 'qualifications';
+						$this->_loadSubView($templateName);
+					}
+						
 					break;
 			}
 		}
 		else {
 			//echo $param . ' Found not links';
 			$this->_menu_team_view($param);
+			$templateName .= 'menuList';
+			$this->_loadSubView($templateName);
 		}
 		
 	}
 	
+	private function _loadSubView($subViewName) {
+		$this->data['menu_prefix'] = NULL;
+		$this->data['subview'] = $subViewName;
+		$this->load->view('_main_layout', $this->data);
+	}
+	
 	private function _menu_team_view($name) {
-		$menuTeamTemplate = 'templates/menuList';
-		
 		$where = array('lang_id' => $this->data['lang_id'], 'linkAddr' => $name);
 		
 		$termMenuID = $this->Navigation_M->get_by($where, TRUE)->id;
 		
-		$whereList = array('lang_id' => $this->data['lang_id'], 'parent_id' => $termMenuID);
+		$whereList = array(
+				'lang_id' => $this->data['lang_id'], 
+				'parent_id' => $termMenuID);
 		$this->data['menu_team_list'] = $this->Navigation_M->get_by($whereList, FALSE);
-		
-		$this->data['menu_prefix'] = NULL;
-		$this->data['subview'] = $menuTeamTemplate;
-		$this->load->view('_main_layout', $this->data);
 	}
 	
 	private function _product_view($name) {
 		$this->data['productName'] = $name;
-		$where = array('lang_id' => $this->data['lang_id'], 'name' => $name);
+		$where = array(
+				'lang_id' => $this->data['lang_id'], 
+				'name' => $name);
 		
 		// get status name
 		$statu_id = $this->get_foreignKey('Product_M', $where, 'statu_id');
 		$this->data['productStatus'] = $this->Statu_M->get($statu_id)->name;
-				
-		switch ($this->data['flag_iLang']) {
-			case 0:
-				$productTemplate = 'templates/production_cn';
-				break;
-			case 1:
-				$productTemplate = 'templates/production';
-				break;
-			case 2:
-				$productTemplate = 'templates/production_ja';
-				break;
-		}
 		
 		// get image id
 		$img_id = $this->get_foreignKey('Product_M', $where, 'img_id');
@@ -122,50 +146,47 @@ class Page extends Frontend_Controller {
 		$param_id = $this->get_foreignKey('Product_M', $where, 'param_id');
 		// set Porduct parameter
 		$this->data['param_list'] = $this->Parameter_M->get($param_id);
-		
-		$this->data['menu_prefix'] = NULL;
-		$this->data['subview'] = $productTemplate;
-		$this->load->view('_main_layout', $this->data);
 	}
 	
 	private function _company_view($name) {
-		$this->get_article_view('articles', $name, 'article_list', TRUE);
+		$this->get_article_view(
+				'articles', 
+				$name, 
+				'article_list', 
+				TRUE);
 	}
 	
-	private function _contact_view($name) {
-		$contactTemplate = 'templates/contact';
-		
+	private function _contact_view($name) {	
 		$spaceName = $this->get_space($name);
 		
-		$where = array('lang_id' => $this->data['lang_id'], 'name' => $spaceName);
+		$where = array(
+				'lang_id' => $this->data['lang_id'], 
+				'name' => $spaceName);
 
 		$this->data['contact'] = $this->Contact_M->get_by($where, TRUE);
-		
-		$this->data['menu_prefix'] = NULL;
-		$this->data['subview'] = $contactTemplate;
-		$this->load->view('_main_layout', $this->data);
 	}
 	
 	private function _about_view($name) {
-		$qualTemplate = 'templates/qualifications';
-		$strFlag = '2';
+		$iFlag = 2;
 		preg_match('/^qual\w+/i', $name, $matches);
 		if(count($matches))
-			$strFlag = '0'; // Aptitude - flag = 0
+		    $iFlag = 0; // Aptitude - flag = 0
 		else
-			$strFlag = '1';	// Technique - flag = 1
+		    $iFlag = 1; // Technique - flag = 1
 		
 		// Qualifications table
 		$col = 'qualifications.*, img.name as imgName';
-		$where = array('qualifications.lang_id' => $this->data['lang_id'], 'qualifications.flag' => $strFlag);
+		$where = array(
+				'qualifications.lang_id' => $this->data['lang_id'], 
+				'qualifications.flag' => strval($iFlag));
 		
 		$joinCond = 'qualifications.img_id = img.id';
 		
-		$this->data['certificates'] = $this->Qualification_M->get_join_table($col, 'images img', $joinCond, $where);
-		
-		$this->data['menu_prefix'] = NULL;
-		$this->data['subview'] = $qualTemplate;
-		$this->load->view('_main_layout', $this->data);
+		$this->data['certificates'] = 
+			$this->Qualification_M->get_join_table(
+					$col, 
+					'images img', 
+					$joinCond, $where);
 	}
 	
 }
